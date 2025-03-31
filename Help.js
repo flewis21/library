c59ae39006b458fc2ae1a717308b1720753a02cb
univ <f.lewis20.class18@outlook.com>
2025-03-31 21:09:44 +0000

    Help.js

--- Help.js
+++ Help.js
@@ -802,9 +802,7 @@ var misSt = function (func, someArgs) {
   // : console.error("funcDos = " + typeof funcDos);
   var argsX = [];
   var content = [];
-  var keys = [[funcUno].concat(someArgs ? " " + funcDos : "")]
-    .toString()
-    .split(",");
+  var keys = [[funcUno].concat(someArgs ? funcDos : "")].toString().split(",");
   keys.forEach((pro) => {
     var bPro = crmT(pro);
     if (bPro >= 0) {
