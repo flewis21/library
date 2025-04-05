4cc74b2f87a982c6e9d4c07aa41de575d6ead08b
univ <f.lewis20.class18@outlook.com>
2025-04-05 16:20:34 +0000

    boilerPlate.js

--- boilerPlate.js
+++ boilerPlate.js
@@ -229,10 +229,13 @@ function objectOfS(keys, values, time) {
       if (
         innerKey === "args" &&
         typeof innerValue === "object" &&
-        innerValue !== null &&
-        innerValue.parameters
+        innerValue !== null
       ) {
-        acc[key][innerKey] = innerValue.parameters;
+        if (innerValue.name && innerValue.parameters) {
+          acc[key][innerKey] = [innerValue.name, ...innerValue.parameters];
+        } else {
+          acc[key][innerKey] = [innerValue.name];
+        }
       } else {
         acc[key][innerKey] = innerValue;
       }
