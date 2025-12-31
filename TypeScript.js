09f1e06b0bee0517ac4764f7e8573e592a783f15
univ <f.lewis20.class18@outlook.com>
2025-12-31 01:14:04 +0000

    Pulled latest changes from Apps Script editor

--- TypeScript.js
+++ TypeScript.js
@@ -1,87 +1,110 @@
+function isTypeScript(typePassedIn) {
+  var mapTypes = {};
+  for (var key in typePassedIn) {
+    mapTypes[key] = typePassedIn[key];
+  }
+  return mapTypes
+}
+
 function isValidKeys(v) {
-  [isOmit(v, k)] ? isExclude(v[k], null) : Array(isOmit(v, k));
-  // if ({a: })
+  var isExcludeValue = isOmit(v, "1")
+  isExcludeValue = isOmit(isExcludeValue, "2")
+  isExcludeValue = isOmit(isExcludeValue, "3")
+  var isValueOfTypePassedIn = isValidDoubleObject(isExcludeValue)
+  if (isValueOfTypePassedIn) {
+    var isInferredTruthy = isTruthy(isValueOfTypePassedIn)
+    if (isInferredTruthy){
+      return isValueOfTypePassedIn
+    }
+  }
+}
+
+function isMapped(t, v) {
+  var mapKeys = {};
+  for (var key in t) {
+    mapKeys[key] = v || typeof t[key];
+  }
+  return mapKeys
 }
 
 function isTruthy(t) {
-  if (
-    typeof t === null ||
-    typeof t === "undefined" ||
-    typeof t === false ||
-    t === 0 ||
-    t === ""
-  ) {
-    return false;
-  } else {
-    return true;
+  if (typeof t === null || typeof t === "undefined" || typeof t === false || t === 0 || t === "") {
+    return false
+  }
+  else {
+    return true
   }
 }
 
 function isValidDoubleObject(t) {
-  var inferObj = {};
-  for (var key in t) {
-    inferObj[key] = typeof t[key];
-  }
-  if (Object.keys(inferObj).length === 2) {
-    var objVal = Object.values(inferObj);
-    if (objVal[0] === objVal[1]) {
-      return t;
-    } else {
-      return null;
+  // var validKeys = isMapped(t);
+  var validList =  Object.values(t);
+  if (validList.length === 2) {
+    var valid1 = validList[0];
+    var valid2= validList[1];
+    if (typeof valid1 === typeof valid2) {
+      return typeof valid1
+    }
+    else {
+      return
     }
   }
-  return null;
+  return
 }
 
 function isKeyOf(t) {
-  var keyList = Object.keys(t);
-  return keyList;
+  var keyList = {}//Object.keys(t);
+  for (var key in t) {
+    keyList[key] = key;
+  }
+  return keyList
 }
 
-function isOmit(t, k) {
-  var tKeys = isKeyOf(t);
-  // var excludeList = isExclude(t, k)
-  var valuesList = isPick(t, k);
-  return valuesList;
-  // if (tKeys.includes(k)) {
-  //   var omitList = isPick()
-  // }
+function isPropertyOf(t) {
+  var propertyList = isMapped(t);
+  return propertyList 
+}
+
+function isOmit(t,k) {
+  var valuesList = isPick(t, k)
+  return valuesList
 }
 
 function isExclude(t, k) {
   var keyList = Object.keys(t);
   var excludeList = [];
-  Array.isArray(keyList)
-    ? keyList.forEach((e, i) => {
-        if (!e.includes(k)) {
-          excludeList.push(e);
-        }
-      })
-    : Array(keyList).forEach((e, i) => {
-        if (!e.includes(k)) {
-          excludeList.push(e);
-        }
-      });
-  return excludeList;
+  Array.isArray(keyList)? keyList.forEach((e, i) => {
+      if (!e.includes(k)) {
+        excludeList.push(e)
+      }
+    }):Array(keyList).forEach((e, i) => {
+      if (!e.includes(k)) {
+        excludeList.push(e)
+      }
+    });
+  return excludeList
 }
 
 function isPick(t, k) {
   var excludeList = isExclude(t, k);
-  var valuesList = [];
+  var valuesList = {};
   for (var key in t) {
     if (excludeList.includes(key)) {
-      valuesList.push(t[key]);
+      valuesList[key] = t[key]
     }
   }
-  return valuesList;
+  return valuesList  
 }
 
 function geneicType() {
-  var exampleObjectType = {
+  var exampleObjectType 
+  = {
     a: "string",
     b: 123,
-    c: ["string"],
-  };
-  var noB = isValidDoubleObject(isPick(exampleObjectType, "b"));
-  return;
+    c: ["string"]
+    }
+    // var kee = isKeyOf(exampleObjectType)
+    // var tee = isMapped(isOmit(exampleObjectType, "b"))
+    var noB = isValidKeys(zuluOrder)
+    return
 }
